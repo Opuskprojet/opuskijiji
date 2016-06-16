946f9147e88aea089f21e00870fff293ca84bc72
Tanguy Buchier <tanguy.buchier@gmail.com>
2016-06-16 11:47:39 -0400

    ajout class entity view

---
+++ src/main/java/ca/uSherbrooke/gegi/opusK/shared/entity/Annonces_opusk.java
@@ -0,0 +1,7 @@
+package ca.uSherbrooke.gegi.opusK.shared.entity;
+
+/**
+ * Created by tanguy on 16/06/16.
+ */
+public class Annonces_opusk {
+}
--- src/main/java/ca/uSherbrooke/gegi/opusK/shared/entity/KView.java
+++
@@ -1,7 +0,0 @@
-package ca.uSherbrooke.gegi.opusK.shared.entity;
-
-/**
- * Created by tanguy on 09/06/16.
- */
-public class KView {
-}
