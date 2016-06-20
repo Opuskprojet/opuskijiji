eb2d0b4c8476643ac483eb62b7551a48bdbbd0fe
Tanguy Buchier <tanguy.buchier@gmail.com>
2016-06-20 15:28:17 -0400

    ajout de l'entité faite par Joel

--- src/main/java/ca/uSherbrooke/gegi/opusK/server/dispatch/RecupFormHandler.java
+++ src/main/java/ca/uSherbrooke/gegi/opusK/server/dispatch/RecupFormHandler.java
@@ -1,7 +1,10 @@
 package ca.uSherbrooke.gegi.opusK.server.dispatch;
 
+import ca.uSherbrooke.gegi.commons.core.server.utils.UserSession;
 import ca.uSherbrooke.gegi.opusK.shared.dispatch.RecupFormAction;
 import ca.uSherbrooke.gegi.opusK.shared.dispatch.RecupFormResult;
+import ca.uSherbrooke.gegi.persist.dao.Dao;
+import com.google.inject.Inject;
 import com.gwtplatform.dispatch.rpc.server.ExecutionContext;
 import com.gwtplatform.dispatch.rpc.server.actionhandler.ActionHandler;
 import com.gwtplatform.dispatch.shared.ActionException;
@@ -13,10 +16,20 @@ import java.util.List;
  */
 public class RecupFormHandler implements ActionHandler<RecupFormAction,RecupFormResult>{
 
+    @Inject
+    Dao myDao;
+
+    @Inject
+    UserSession session;
+
     @Override
     public RecupFormResult execute(RecupFormAction action, ExecutionContext executionContext) throws ActionException {
 
+        int idUser = session.getUserId();
+
         List<String> datas = action.getDatas();
+
+
         // entity set champs
         // dao.persist(entity)
 
--- src/main/java/ca/uSherbrooke/gegi/opusK/server/dispatch/SearchHandler.java
+++ src/main/java/ca/uSherbrooke/gegi/opusK/server/dispatch/SearchHandler.java
@@ -1,7 +1,10 @@
 package ca.uSherbrooke.gegi.opusK.server.dispatch;
 
+import ca.uSherbrooke.gegi.commons.core.server.utils.UserSession;
 import ca.uSherbrooke.gegi.opusK.shared.dispatch.SearchAction;
 import ca.uSherbrooke.gegi.opusK.shared.dispatch.SearchResult;
+import ca.uSherbrooke.gegi.persist.dao.Dao;
+import com.google.inject.Inject;
 import com.gwtplatform.dispatch.rpc.server.ExecutionContext;
 import com.gwtplatform.dispatch.rpc.server.actionhandler.ActionHandler;
 import com.gwtplatform.dispatch.shared.ActionException;
@@ -10,10 +13,21 @@ import com.gwtplatform.dispatch.shared.ActionException;
  * Created by tanguy on 10/06/16.
  */
 public class SearchHandler implements ActionHandler<SearchAction,SearchResult> {
+
+    @Inject
+    Dao myDao;
+
+    @Inject
+    UserSession session;
+
     @Override
     public SearchResult execute(SearchAction searchAction, ExecutionContext executionContext) throws ActionException {
         String searchQuery = searchAction.getSearchQuery();
         String categorie = searchAction.getCategorie();
+
+       // session.getUserId();
+
+        //myDao.persist();
 
         // DAO find ..
         // New List<Entity> ...
